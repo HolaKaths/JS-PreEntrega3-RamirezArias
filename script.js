da9768e1164f5c200e9f5152e2aac233bd8cab9b
Katheryn Ramirez <holakaths@gmail.com>
2023-03-30 16:28:34 +0200

    Inicio - gráfico de barras

--- script.js
+++ script.js
@@ -3,109 +3,68 @@ function calcularEdad() {
     const nombre = document.getElementById("nombre").value;
     const edad = document.getElementById("edad").value;
     const tamano = document.getElementById("tamano").value;
-  
+
     let edadHumana = 0;
     let peso = 0;
-  
+
     animal === "perro" ? (
         edad === 1 ? edadHumana = 15 :
-        edad === 2 ? edadHumana = 24 :
-        edadHumana = 24 + (edad - 2) * 4,
+            edad === 2 ? edadHumana = 24 :
+                edadHumana = 24 + (edad - 2) * 4,
         peso = tamano === "pequeno" ? (edad <= 12 ? edad * 2 : 24 + (edad - 12) * 1) :
-               tamano === "mediano" ? (edad <= 12 ? edad * 2.5 : 30 + (edad - 12) * 1.5) :
-               edad <= 12 ? edad * 4 : 54 + (edad - 12) * 2
-      ) : animal === "gato" ? (
+            tamano === "mediano" ? (edad <= 12 ? edad * 2.5 : 30 + (edad - 12) * 1.5) :
+                edad <= 12 ? edad * 4 : 54 + (edad - 12) * 2
+    ) : animal === "gato" ? (
         edad === 1 ? edadHumana = 15 :
-        edad === 2 ? edadHumana = 24 :
-        edadHumana = 24 + (edad - 2) * 4,
+            edad === 2 ? edadHumana = 24 :
+                edadHumana = 24 + (edad - 2) * 4,
         peso = tamano === "pequeno" ? 2.5 * edad + 1.5 :
-               tamano === "mediano" ? 3.5 * edad + 1.5 :
-               5.5 * edad + 1.5
-      ) : null;
-      
-  
+            tamano === "mediano" ? 3.5 * edad + 1.5 :
+                5.5 * edad + 1.5
+    ) : null;
+
     const resultado = document.getElementById("resultado");
     resultado.innerHTML = `La edad de tu ${animal} ${nombre} en años humanos es ${edadHumana} y su peso ideal estimado a esa edad sería ${peso} kg.`;
-  }
-  
-  function calcularEdadMascotas() {
-    function calcularEdad() {
-      const animal = document.getElementById("animal").value;
-      const nombre = document.getElementById("nombre").value;
-      const edad = document.getElementById("edad").value;
-      const tamano = document.getElementById("tamano").value;
-  
-      let edadHumana = 0;
-      let peso = 0;
-  
-      animal === "perro" ? (
-        edad === 1 ? edadHumana = 15 :
-        edad === 2 ? edadHumana = 24 :
-        edadHumana = 24 + (edad - 2) * 4,
-        tamano === "pequeno" ? peso = edad <= 12 ? edad * 2 : 24 + (edad - 12) * 1 :
-        tamano === "mediano" ? peso = edad <= 12 ? edad * 2.5 : 30 + (edad - 12) * 1.5 :
-        peso = edad <= 12 ? edad * 4 : 54 + (edad - 12) * 2
-      ) : (
-        edad === 1 ? edadHumana = 15 :
-        edad === 2 ? edadHumana = 24 :
-        edadHumana = 24 + (edad - 2) * 4,
-        tamano === "pequeno" ? peso = 2.5 * edad + 1.5 :
-        tamano === "mediano" ? peso = 3.5 * edad + 1.5 :
-        peso = 5.5 * edad + 1.5
-      );
-      
-  
-      const resultado = document.getElementById("resultado");
-      resultado.innerHTML = `La edad de tu ${animal} ${nombre} en años humanos es ${edadHumana} y su peso ideal estimado a esa edad sería ${peso} kg.`;
-    }
-  
-    function calcularEdadMascotas() {
-      const numMascotas = parseInt(prompt("Ingresa el número de mascotas a calcular:"));
-      let i = 0;
-  
-      while (i < numMascotas) {
+}
+
+function calcularEdadMascotas() {
+    const numMascotas = parseInt(prompt("Ingresa el número de mascotas a calcular:"));
+    let i = 0;
+
+    while (i < numMascotas) {
         calcularEdad();
         i++;
-      }
     }
-  
-  
-    let i = 0;
-  
-    while (i < numMascotas) {
-      calcularEdad();
-      i++;
-    }
-  }
-  
-  function calcularIMC() {
+}
+
+function calcularIMC() {
     // Obtener los valores de peso y altura
     const peso = document.getElementById('peso').value;
     const altura = document.getElementById('altura').value;
-  // Calcular el IMC
-  const imc = peso / ((altura / 100) ** 2);
-  
-  // Mostrar el resultado en la página
-  const resultado = document.getElementById('resultado2');
-  resultado.textContent = imc.toFixed(1);
-  
-  // Evaluar el resultado del IMC
-  resultado.textContent += imc < 18.5 ? " - Bajo peso" 
-  : imc >= 18.5 && imc < 25 ? " - Peso normal" 
-  : imc >= 25 && imc < 30 ? " - Sobrepeso" 
-  : imc >= 30 && imc < 35 ? " - Obesidad grado 1" 
-  : imc >= 35 && imc < 40 ? " - Obesidad grado 2"
-  : " - Obesidad grado 3";
+    // Calcular el IMC
+    const imc = peso / ((altura / 100) ** 2);
 
-  
-  function addNewRow() {
+    // Mostrar el resultado en la página
+    const resultado = document.getElementById('resultado2');
+    resultado.textContent = imc.toFixed(1);
+
+    // Evaluar el resultado del IMC
+    resultado.textContent += imc < 18.5 ? " - Bajo peso"
+        : imc >= 18.5 && imc < 25 ? " - Peso normal"
+            : imc >= 25 && imc < 30 ? " - Sobrepeso"
+                : imc >= 30 && imc < 35 ? " - Obesidad grado 1"
+                    : imc >= 35 && imc < 40 ? " - Obesidad grado 2"
+                        : " - Obesidad grado 3";
+}
+
+function addNewRow() {
     let table = document.getElementById("imcData");
     let row = table.insertRow();
     let cell1 = row.insertCell(0);
     let cell2 = row.insertCell(1);
     let cell3 = row.insertCell(2);
     let cell4 = row.insertCell(3);
-  
+
     let nuevaEdad = document.createElement("input");
     nuevaEdad.setAttribute("type", "number");
     nuevaEdad.setAttribute("class", "edad");
@@ -115,7 +74,7 @@ function calcularEdad() {
     nuevaEdad.setAttribute("step", "0.1");
     nuevaEdad.setAttribute("form", "formularioIMC");
     nuevaEdad.setAttribute("required", "");
-  
+
     let nuevoPeso = document.createElement("input");
     nuevoPeso.setAttribute("type", "number");
     nuevoPeso.setAttribute("class", "peso");
@@ -125,7 +84,7 @@ function calcularEdad() {
     nuevoPeso.setAttribute("step", "0.1");
     nuevoPeso.setAttribute("form", "formularioIMC");
     nuevoPeso.setAttribute("required", "");
-  
+
     let nuevaAltura = document.createElement("input");
     nuevaAltura.setAttribute("type", "number");
     nuevaAltura.setAttribute("class", "altura");
@@ -134,43 +93,79 @@ function calcularEdad() {
     nuevaAltura.setAttribute("max", "200");
     nuevaAltura.setAttribute("form", "formularioIMC");
     nuevaAltura.setAttribute("required", "");
-  
+
     cell1.appendChild(nuevaEdad);
     cell2.appendChild(nuevoPeso);
     cell3.appendChild(nuevaAltura);
     cell4.innerHTML = "";
-  }
-  
-  
-  function calcularTabla() {
-  
-  }
-  
-  function leerTabla() {
+}
+
+
+function calcularTabla() {
+
+}
+
+function leerTabla() {
     const valoresTabla = document.querySelectorAll("#imcData tr");
     let listaObjetosIMC = [];
     for (let index = 1; index < valoresTabla.length; index++) {
-      let edad = valoresTabla[index].getElementsByTagName("input")['edad'].value;
-      let peso = valoresTabla[index].getElementsByTagName("input")['peso'].value;
-      let altura = valoresTabla[index].getElementsByTagName("input")['altura'].value;
-      let imc = 0;
-      listaObjetosIMC[index - 1] = { edad, peso, altura, imc }
+        let edad = valoresTabla[index].getElementsByTagName("input")['edad'].value;
+        let peso = valoresTabla[index].getElementsByTagName("input")['peso'].value;
+        let altura = valoresTabla[index].getElementsByTagName("input")['altura'].value;
+        let imc = 0;
+        listaObjetosIMC[index - 1] = { edad, peso, altura, imc }
     }
     listaObjetosIMC = calcularIMCTabla(listaObjetosIMC)
-  
+
     document.querySelectorAll("#imcData tr")
-  
+
     for (let index = 1; index < valoresTabla.length; index++) {
-      valoresTabla[index].lastElementChild.innerHTML = listaObjetosIMC[index - 1].imc
+        valoresTabla[index].lastElementChild.innerHTML = listaObjetosIMC[index - 1].imc
     }
-  }
-  
-  function calcularIMCTabla(listaObjetosIMC) {
+}
+
+function calcularIMCTabla(listaObjetosIMC) {
     for (let index = 0; index < listaObjetosIMC.length; index++) {
-      let aux = listaObjetosIMC[index];
-      aux.imc = aux.peso / ((aux.altura / 100) ** 2);
-      listaObjetosIMC[index] = aux;
+        let aux = listaObjetosIMC[index];
+        aux.imc = aux.peso / ((aux.altura / 100) ** 2);
+        listaObjetosIMC[index] = aux;
     }
     return listaObjetosIMC
-  }
-  
+}
+
+// Gráfico de barras
+
+const data = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [
+        {
+            label: 'Sales',
+            backgroundColor: 'rgba(54, 162, 235, 0.2)',
+            borderColor: 'rgba(54, 162, 235, 1)',
+            borderWidth: 1,
+            hoverBackgroundColor: 'rgba(54, 162, 235, 0.4)',
+            hoverBorderColor: 'rgba(54, 162, 235, 1)',
+            data: [65, 59, 80, 81, 56, 55, 40],
+        },
+    ],
+};
+
+const options = {
+    scales: {
+        yAxes: [
+            {
+                ticks: {
+                    beginAtZero: true,
+                },
+            },
+        ],
+    },
+};
+
+
+const ctx = document.getElementById('myChart').getContext('2d');
+const myChart = new Chart(ctx, {
+  type: 'bar',
+  data: data,
+  options: options,
+});
